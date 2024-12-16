5b9438119f3ca9d8d25d66a91c90c36b01d46be4
Máté Fehér <c9fehmat@vasvari.org>
2024-12-16 08:56:12 +0100

    swag

--- app.js
+++ app.js
@@ -1,52 +1,83 @@
-import express from "expresss";
+import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import sqlite3 from "sqlite3";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.listen(3020);
+app.use(bodyParser.json());
 
-PORT = 3020
-
-const startServer = () => {
-    app.listen(PORT)
-    console.log(`The server is running on localhost:${PORT}`)
-}
-const db = new sqlite3.Database("./cars.sqlite")
+const db = new sqlite3.Database(`./db.sqlite`);
 
 db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS cars (id PRIMARY KEY AUTO INCREMENT, brand TEXT, model TEXT, color TEXT, year INTEGER NOT NULL)")
+  db.run(
+    "CREATE TABLE IF NOT EXISTS cars (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, brand TEXT NOT NULL, model TEXT, color TEXT, year INTEGER);"
+  );
 });
 
-app.get("./cars", (req, res) =>{
-    db.all("SELECT * FROM cars;", [], (err, rows) => {
-        if (err){
-            res.status(500).json({error: err.message})
-        }
-        else {
-            res.json(200);
-        }
-    })
+app.get("./cars", (req, res) => {
+  db.all("SELECT * FROM cars", [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+app.post("./cars", (req, res) => {
+  const { brand, model, color, year } = req.body;
+  if (!brand) {
+    return res.status(400).json({ error: "Brand is required" });
+  }
+  db.run(
+    "INSERT INTO cars (brand, model, color, year) VALUES (?, ?, ?, ?)",
+    [brand, model, color, year],
+    function (err) {
+      if (err) {
+        res.status(500).json({ error: err.message });
+      } else {
+        res.status(201).json({ id: this.lastID });
+      }
+    }
+  );
 });
 
 app.get("./cars/:id", (req, res) => {
-    db.all("SELECT * FROM cars WHERE id = ?;", [], (err, rows) => {
-        if (err){
-            res.status(500).json({error: err.message})
-        }
-        else {
-            res.json(200)
-        }
-    })
+  const { id } = req.params.id;
+  db.get("SELECT * FROM cars WHERE id = ?", [id], (err, row) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else if (!row) {
+      res.status(404).json({ error: "car not found" });
+    } else {
+      res.json(row);
+    }
+  });
 });
 
-app.post('./cars', (req, res) => {
-    const (brand, model, color, year) = req.body;
-    if (!brand){
-        return res.status(400).json({error: "No Brand Given"})
+app.put("./cars/:id", (res, req) => {
+  const { id } = req.params.id;
+  const { brand, model, color, year } = req.body;
+  if (!brand) {
+    return res.status(400).json({ error: "Brand is required" });
+  }
+  db.run(
+    "UPDATE cars SET brand = ?, model = ?, color = ?, year = ? WHERE id = ?",
+    [brand, model, color, year, id],
+    function (err) {
+      if (err) {
+        res.status(500).json({ error: err.message });
+      } else if (this.changes === 0) {
+        res.status(201).json({ id: this.lastID });
+      } else {
+        res.status(400).json({error: err.message})
+      }
     }
-    else {
-        res.json(200);
-    }
+  );
 });
+
+app.delete("./cars/:id", (req, res) => {
+    const {id} = req.params.id;
+    
+})
